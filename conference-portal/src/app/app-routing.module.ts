86662f9738ab901cda7a5dbfa8caa1ebbd262902
chriaha <chriaha@stud.ntnu.no>
2023-01-11 20:33:23 +0000

    added components

--- conference-portal/src/app/app-routing.module.ts
+++ conference-portal/src/app/app-routing.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { SpeakersComponent } from './speakers/speakers.component';
+import { CommitteesComponent } from './committees/committees.component';
+import { PapersComponent } from './papers/papers.component';
+import { DatesComponent } from './dates/dates.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ProgramComponent } from './program/program.component';
+import { PhotosComponent } from './photos/photos.component';
+import { PreviousComponent } from './previous/previous.component';
+import { ContactComponent } from './contact/contact.component';
+import { SupportComponent } from './support/support.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'speakers', component: SpeakersComponent},
+  { path: 'papers', component: PapersComponent},
+  { path: 'committees', component: CommitteesComponent},
+  { path: 'dates', component: DatesComponent},
+  { path: 'program', component: ProgramComponent},
+  { path: 'registration', component: RegistrationComponent},
+  { path: 'photos', component: PhotosComponent},
+  { path: 'previous', component: PreviousComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'support', component: SupportComponent}
 ];
 
 @NgModule({
---
+++ conference-portal/src/app/previous/previous.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-previous',
+  templateUrl: './previous.component.html',
+  styleUrls: ['./previous.component.scss']
+})
+export class PreviousComponent {
+
+}

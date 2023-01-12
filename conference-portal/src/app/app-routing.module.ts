36288e6ff5f456a2f73b06b424e97659ebeb799a
Lennart Hoyer <lennart.hoyer@gmail.com>
2023-01-12 00:24:14 +0000

    Auth guard and snackbar message service Create auth functionality #3

--- conference-portal/src/app/app-routing.module.ts
+++ conference-portal/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { SpeakersComponent } from './speakers/speakers.component';
+import { AuthGuard } from './user/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
-  { path: 'speakers', component: SpeakersComponent },
+  {
+    path: 'speakers',
+    component: SpeakersComponent,
+    canActivate: [AuthGuard]
+  },
 ];
 
 @NgModule({
